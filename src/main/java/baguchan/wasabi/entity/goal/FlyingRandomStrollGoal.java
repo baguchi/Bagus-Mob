219ca729ba960773a447f1553e081a3c3b315e83
baguchan <shutux500@gmail.com>
2023-03-10 16:51:37 +0900

    remake FlyingRandomStrollGoal

--- src/main/java/baguchan/wasabi/entity/goal/FlyingRandomStrollGoal.java
+++ src/main/java/baguchan/wasabi/entity/goal/FlyingRandomStrollGoal.java
@@ -4,13 +4,14 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.entity.ai.util.AirAndWaterRandomPos;
+import net.minecraft.world.entity.ai.util.HoverRandomPos;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
 
 public class FlyingRandomStrollGoal extends RandomStrollGoal {
 	public FlyingRandomStrollGoal(PathfinderMob p_25734_, double p_25735_) {
-		super(p_25734_, p_25735_, 20);
+		super(p_25734_, p_25735_, 10);
 	}
 
 	@Override
@@ -26,16 +27,7 @@ public class FlyingRandomStrollGoal extends RandomStrollGoal {
 
 			Vec3 vec3 = this.getTargetFlyPos(this.mob, 15, 10);
 			if (vec3 == null) {
-				Vec3 vec32 = this.getPosition();
-				if (vec32 == null) {
-					return false;
-				} else {
-					this.wantedX = vec32.x;
-					this.wantedY = vec32.y;
-					this.wantedZ = vec32.z;
-					this.forceTrigger = false;
-					return true;
-				}
+				return false;
 			} else {
 				this.wantedX = vec3.x;
 				this.wantedY = vec3.y;
@@ -46,30 +38,23 @@ public class FlyingRandomStrollGoal extends RandomStrollGoal {
 		}
 	}
 
-	@Override
-	protected Vec3 getPosition() {
-		Vec3 vec3 = this.mob.getViewVector(0.0F);
-		int i = 8;
-
-		RandomSource randomsource = this.mob.getRandom();
-		double d0 = this.mob.getX() + (double) ((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
-		double d1 = this.mob.getY() + (double) ((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
-		double d2 = this.mob.getZ() + (double) ((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
-
-		if (this.mob.getY() > 160) {
-			d1 = this.mob.getY() - (double) ((randomsource.nextFloat() * 1.2F) * 16.0F);
-		}
-
-		if (this.mob.getY() > 12) {
-			d1 = this.mob.getY() + (double) ((randomsource.nextFloat() * 5.0F) * 16.0F);
-		}
-
-		return new Vec3(d0, d1, d2);
-	}
-
 	@Nullable
 	private Vec3 getTargetFlyPos(PathfinderMob p_260316_, int p_259038_, int p_259696_) {
 		Vec3 vec3 = p_260316_.getViewVector(0.0F);
-		return AirAndWaterRandomPos.getPos(p_260316_, p_259038_, p_259696_, -2, vec3.x, vec3.z, (double) ((float) Math.PI / 2F));
+
+		Vec3 path = AirAndWaterRandomPos.getPos(p_260316_, p_259038_, p_259696_, -2, vec3.x, vec3.z, (double) ((float) Math.PI / 2F));
+
+		Vec3 vec32 = HoverRandomPos.getPos(p_260316_, p_259038_, p_259696_, vec3.x, vec3.z, ((float) Math.PI / 2F), 3, 1);
+
+
+		RandomSource randomsource = p_260316_.getRandom();
+		double d0 = p_260316_.getX() + (double) ((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
+		double d1 = p_260316_.getY() + (double) ((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
+		double d2 = p_260316_.getZ() + (double) ((randomsource.nextFloat() * 2.0F - 1.0F) * 16.0F);
+
+		if (vec32 != null) {
+			return vec32;
+		}
+		return path != null ? path : new Vec3(d0, Math.max(160, d1), d2);
 	}
 }
